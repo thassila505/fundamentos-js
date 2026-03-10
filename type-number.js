a7ab08ed8788f41b28513caf4ff3358baf775240
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-03-10 14:15:19 +0000

    variaveis

--- type-number.js
+++ type-number.js
@@ -1,1 +1,22 @@
+//tipo number
 
+const primeiroNumero = 3;
+const  segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero = segundoNumero
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log (novaOperacao);
+
+//naN -> Not a Number (não é número)
+
+const alura = "Alura"
+console.log(alura * primeiroNumero)
---
+++ type-string.js
@@ -0,0 +1,11 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+
+const senha = "senhaSuperSegura456";
+
+const stringDeNumeros = "3456";
+
+const citacao = "Meu nome é";
+const meuNome = "Thassila";
+
+console.log(citacao + meuNome);
