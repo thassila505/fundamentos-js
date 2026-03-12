bddc8cd9163b25fe09d59c909bb8fe734f6bf155
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-03-12 15:02:47 +0000

    comparacao

---
+++ comparacoes.js
@@ -0,0 +1,15 @@
+// == (Comparação implícita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto)
+
+//typeof
+console.log(typeof numero);
+console.log(typeof texto);
+
+//conversão explicita
+Number()
+String()
+
