e250941bfa4901643779512af5249e3506e63c46
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-03-13 10:47:53 +0000

    template

---
+++ template-strings.js
@@ -0,0 +1,3 @@
+const nome = "Ju";
+const idade = 2026-1881;
+const cidadeDeNascimento = "Pitanga";
---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 20;
+ 
+//Se idadeCliente for maior idadeMinima então faça
+if (idadeCliente >= idadeMinima){
+    console.log("Cerveja");
+}else{
+    console.log("Suco");
+}
+            //condição       "true"         "false"
+console.log(idadeCliente >= idadeMinima? "cerveja" : "suco" );
