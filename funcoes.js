ba5511499b945b4d81a1f36811494df750652e3b
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-03-20 10:48:01 +0000

    function

---
+++ funcoes.js
@@ -0,0 +1,10 @@
+//let x = "";
+//x = "oi";
+//console.log(x);
+
+
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+imprimeTexto("Oi mundo")
--- template-strings.js
+++ template-strings.js
@@ -1,3 +1,8 @@
 const nome = "Ju";
 const idade = 2026-1881;
 const cidadeDeNascimento = "Pitanga";
+
+// const apresentacao = "Meu nome é " + nome + "e nasci na cidade de" + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+console.log(apresentacao)
