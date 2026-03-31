f9de522221a6c5394e845be27883195f1d872a82
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-03-31 15:06:02 +0000

    argumentos

--- parametros.js
+++ parametros.js
@@ -5,3 +5,15 @@ function soma(numero1, numero2){
 console.log(soma( 2,2));
 console.log(soma( 245,20));
 console.log(soma( -500, 60));
+
+function nomeIdade(idade, idade){
+    return `meu nome é ${soma} e minha idade é${idade}`
+}
+
+console.log(nomeIdade("Alexandre", 44))
+
+function multiplica(numero1, numero2){
+    return numero1 * numero2;
+}
+
+console.log(multiplica(soma(4,5), soma(3, 3)))
