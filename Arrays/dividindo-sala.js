57e4a2804e2f000e2eed882241108245e5eee7ec
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-05-08 11:22:12 -0300

    slice

---
+++ Arrays/dividindo-sala.js
@@ -0,0 +1,7 @@
+const alunos =['Alana', 'Amanda O', 'Amanda L', 'Manu', 'Gaby', 'Gustavo', 'Henrique', 'Jose S', 'Jose L', 'Kaio', 'Lorena', 'Luis F', 'Matheus', 'Nicolas', 'Pietro', 'Rafael B', 'Rafael M', 'Thassila', 'Vitoria']
+
+const sala1 = alunos.slice(0, 10);
+const sala2 = alunos.slice(10);
+
+console.log(sala1);
+console.log(sala2);
