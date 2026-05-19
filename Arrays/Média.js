bf3508c3e0d3a6448b4fc6b062af48bda3879bca
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-05-19 11:59:14 -0300

    novo

--- Arrays/Média.js
+++ Arrays/Média.js
@@ -1,7 +1,10 @@
-const nota1 = 10;
-const nota2 = 6.5;
-const nota3 = 8;
-const nota4 = 7.5;
+//const nota1 = 10;
+//const nota2 = 6.5;
+//const nota3 = 8;
+//const nota4 = 7.5;
+
+const notas = [10, 6.5, 8, 7,5];
 
 const media = (nota1, nota2, nota3, nota4)/4
 console.log(media);
+console.log(notas[3]);
