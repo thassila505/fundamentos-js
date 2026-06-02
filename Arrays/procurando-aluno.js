ac6b5b5aa389f44bc2acd8e7a5b77a37a58605c9
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-06-02 13:33:53 +0000

    refatoração

--- Arrays/procurando-aluno.js
+++ Arrays/procurando-aluno.js
@@ -5,8 +5,19 @@ const medias = [10, 8, 7.5, 9];
 const listasDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno){
-    if(listaDeAlunosEMedias[0].includes(aluno))[
-        console.log(`Aluno não cadastrado`)
-    ]
+    if(listasDeAlunosEMedias[0].includes(aluno)){
+         const [alunos, medias] = listasDeAlunosEMedias;
+
+        const indice = alunos.index0f(aluno);
+
+        const mediaDoAlunon= medias[indice];
+
+        console.log(`${aluno} tem média ${mediaDoAluno}.`);
+    }else{
+        console.log(`Aluno não cadastrado`);
+
+    }
+    
 }
-exibeNomeENota("Rafael");
+
+exibeNomeENota("Ana");
