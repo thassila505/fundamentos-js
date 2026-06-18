8f85583ce1660687a48601d41a896ddc8b4e7a6d
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-06-18 15:02:32 +0000

    forEach

---
+++ Arrays/media-forEach.js
@@ -0,0 +1,9 @@
+const notas = [10, 6.5, 8, 7.5];
+
+notas.forEach(function(){
+    console.log("Oi");
+});                                      
+
+const media = somaDasNotas / notas.longht;
+
+console.log(`a media das notas é ${media}.`);
