33404a85c4785e3e719169fca35c95f960b2fe20
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-06-30 13:35:02 +0000

    map.nomes

---
+++ Arrays/map.nomes.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana Julia", "Caio vinicius", "BIA silva"];
+
+const nomesPadronizados = nomes.map((nomes) => {
+    return nome.toUpperCase();
+});
+
+console.log(nomesPadronizados);
