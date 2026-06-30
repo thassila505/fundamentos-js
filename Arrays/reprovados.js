54c64929b0127deeea35e1cd28634b925e62aba7
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-06-30 14:59:47 +0000

    reprovados

--- Arrays/reprovados.js
+++ Arrays/reprovados.js
@@ -2,7 +2,7 @@
 
  const medias = [7, 4.5, 8, 7.5];
 
- const reprovados = alunos.filter((aluno, indice) => {
+ const reprovados = alunos.filter((_, indice) => {
     return medias [indice] < 7;
  })
 
