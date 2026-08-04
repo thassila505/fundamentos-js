bac329973de32251e821b3430ec15ae78dd40f5b
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-08-04 15:08:43 +0000

    acesso.

---
+++ objetos/objetos.js
@@ -0,0 +1,10 @@
+const listaCPF = [1111111, 222222, 3333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "11112222333"];
+
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    idade = 16,
+    Nome= Thassila,
+};
