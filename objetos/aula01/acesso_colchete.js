03b39d95e6414027c470ee04e5eaf2cbeb838876
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-08-28 11:27:31 +0000

    tipos

---
+++ objetos/aula01/acesso_colchete.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "Thassila",
+    idade: 16,
+    cpf: "1122233345",
+    email: "thassila@dominio.com",
+};
+
+console.log(`O nome do cliente é $ {cliente["nome"]} e essa pessoa tem $ {cliente["idade"] anos.`);
+
+const chaves = ["nome", "idade", "cpf", "email"];
+
+chaves.forEach((chave) => {
+    console.log(`A chave ${chave} tem valor ${cliente [chave]}`);
+});
---
+++ objetos/aula02/tipos.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Thassila",
+    idade: 16,
+    email: "thassila@filma.com",
+    telefone: ["42555991323", "42999885544"],
+}
+
+console.log(cliente);
